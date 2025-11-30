5e493d702a01ae5738f06cdc0f5ea1a8f69e4fc9
darksaid98 <darksaid9889@gmail.com>
2025-11-30 14:04:16 +0100

    fix: replace deprecated setmeta method

--- src/main/java/io/github/alathra/boltux/api/BoltUXAPI.java
+++ src/main/java/io/github/alathra/boltux/api/BoltUXAPI.java
@@ -1,11 +1,12 @@
 package io.github.alathra.boltux.api;
 
-import io.github.alathra.boltux.BoltUX;
 import io.github.alathra.boltux.config.Settings;
 import io.github.alathra.boltux.hook.Hook;
 import io.github.alathra.boltux.utility.Logger;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.CustomModelData;
+import io.papermc.paper.datacomponent.item.ItemLore;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class BoltUXAPI {
 
@@ -68,6 +69,7 @@ public class BoltUXAPI {
      * Get if the provided ItemStack is a lock
      * @return If the ItemStack consists of lock items
      */
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     public static boolean isLockItem(ItemStack itemStack) {
         return itemStack.isSimilar(getLockItem());
     }
@@ -77,12 +79,13 @@ public class BoltUXAPI {
      * @return The default (native) lock item if no item plugin is specified
      */
     private static ItemStack getDefaultLockItem() {
-        ItemStack lockItem = new ItemStack(Settings.getDefaultLockItemMaterial());
-        ItemMeta lockMeta = lockItem.getItemMeta();
-        lockMeta.displayName(Settings.getDefaultLockItemDisplayName());
-        lockMeta.lore(Settings.getDefaultLockItemLore());
-        lockMeta.setCustomModelData(Settings.getDefaultLockItemCustomModelData());
-        lockItem.setItemMeta(lockMeta);
-        return lockItem;
+        final ItemStack item = new ItemStack(Settings.getDefaultLockItemMaterial());
+        item.setData(DataComponentTypes.CUSTOM_NAME, Settings.getDefaultLockItemDisplayName());
+        item.setData(DataComponentTypes.LORE, ItemLore.lore(Settings.getDefaultLockItemLore()));
+        item.setData(DataComponentTypes.CUSTOM_MODEL_DATA, CustomModelData.customModelData()
+            .addFloat(Settings.getDefaultLockItemCustomModelData())
+            .build()
+        );
+        return item;
     }
 }
